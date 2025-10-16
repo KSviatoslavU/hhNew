b2358364977d13ba0cc194a8053956cea10c9bf1
Sviatoslav K <s_korostylev@inbox.ru>
2025-10-16 09:29:32 +0300

    fix: GitHub Pages redirect for SPA

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,16 @@ import "@mantine/core/styles.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/hh">
+    <HashRouter>
       <Provider store={store}>
         <MantineProvider theme={theme}>
           <App />
         </MantineProvider>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
